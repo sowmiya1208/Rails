2ba1dfaeba651afe188439380baefa48128d9570
Sowmiya <102214041+sowmiya1208@users.noreply.github.com>
2025-04-17 22:19:12 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
-
+  config.hosts.clear
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
